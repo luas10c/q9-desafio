fa21c228f459f29dd60e9c72c4bb68d78d6e4e05
Luas1 <luciano.mamonas98@gmail.com>
2020-07-28 05:23:28 -0300

    Update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Button from '~/components/Button';
+
+import Logo from '~/assets/logo.svg';
+
+import { Container, Header, Content, Info } from './styles';
 
 const Home = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <Logo width={177} height={177} fill="#FFFFFF" />
+      </Header>
+      <Content>
+        <Info>
+          Se você for um usuário novo ao prosseguir você conseguirá criar conta.
+          mas se você já é um de nossos usuários aos prosseguir você também
+          conseguirá acessar sua conta usando seu email de cadastro.
+        </Info>
+        <Button>Prosseguir</Button>
+      </Content>
+    </Container>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  padding: 0 18px;
+  background-color: #f97f51;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  justify-content: flex-end;
+  height: 294px;
+  padding-bottom: 36px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const Info = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+  color: #414141;
+  padding: 20px;
+  margin: 36px 0;
+  background-color: #ffffff;
+  border-radius: 4px;
+`;
