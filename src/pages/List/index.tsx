e4d9d51cd5a2c28bab29a1a8b30900912389b382
Luas1 <luciano.mamonas98@gmail.com>
2020-07-29 06:14:57 -0300

    Add List

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import api from '~/services/api';
 
-import { Container } from './styles';
+import Spinner from '~/components/Spinner';
+import Gallery from '~/components/Gallery';
+import Navigation from '~/components/Navigation';
 
-const List = () => {
-  return <Container />;
+import navigationItems from './navigationItems';
+
+import { Container, Content, Footer } from './styles';
+
+const List: React.FC<any> = () => {
+  const [breed, setBreed] = useState('');
+  const [images, setImages] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchImagesByBreed();
+
+    return () => {
+      fetchImagesByBreed();
+    };
+  }, [breed]);
+
+  const fetchImagesByBreed = async () => {
+    setImages([]);
+    try {
+      setIsLoading(true);
+      const { data } = await api.get(`list${breed && `?breed=${breed}`}`, {
+        headers: {
+          Authorization:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmMDUxNTFiMC1iMzUyLTRiZjYtOTc0Ni0yMDAyOWMyMjI5YTciLCJzdWIiOiI1ZjE3NjVjNGM4ZWExODAwMDQyZTJhMTciLCJpYXQiOjE1OTUzNjg5MDAsImV4cCI6MTU5NjY2NDkwMH0.xFi9tmMlOMfYOLXIsqcfWFHwtzNw2QhVm2MF7U-05Lc'
+        }
+      });
+      setImages(data.list);
+      setIsLoading(false);
+    } catch (error) {
+      //
+    }
+  };
+
+  const handleChangeItem = (value: string) => {
+    if (value !== breed) {
+      setBreed(value);
+    }
+  };
+
+  return (
+    <Container>
+      <StatusBar backgroundColor="#FFFFFF" style="dark" translucent={false} />
+      <Content>
+        {isLoading && <Spinner size="large" color="#f97f51" />}
+        {!isLoading && images && <Gallery images={images} />}
+      </Content>
+      <Footer>
+        <Navigation items={navigationItems} onChangeItem={handleChangeItem} />
+      </Footer>
+    </Container>
+  );
 };
 
 export default List;
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #ffffff;
+  align-items: center;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  height: 117px;
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-style: solid;
+  border-color: #aeaeae;
+  z-index: 999999;
+`;
