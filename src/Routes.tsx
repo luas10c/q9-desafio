8d1aad24b820c3e5808a0d24aa72e21c90c94189
Luas1 <luciano.mamonas98@gmail.com>
2020-07-29 01:23:58 -0300

    Update Routes Inital Home

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,14 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from '~/pages/Home';
 import Register from '~/pages/Register';
+import List from '~/pages/List';
 
 const Stack = createStackNavigator();
 
 const Routes = () => (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName="Register" headerMode="none">
+    <Stack.Navigator initialRouteName="Home" headerMode="none">
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="List" component={List} />
     </Stack.Navigator>
   </NavigationContainer>
 );
