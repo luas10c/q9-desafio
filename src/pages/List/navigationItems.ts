87147294644b2810c68c301593514ceacaeb67d7
Luas1 <luciano.mamonas98@gmail.com>
2020-07-29 06:14:36 -0300

    Add Navigation Items

---
+++ src/pages/List/navigationItems.ts
@@ -0,0 +1,29 @@
+import Chihuahua from '~/assets/chihuahua.jpg';
+import Husky from '~/assets/husky.jpg';
+import Pug from '~/assets/pug.jpg';
+import Labrador from '~/assets/labrador.jpg';
+
+const navigationItems = [
+  {
+    name: 'chihuahua',
+    label: 'Chihuahua',
+    image: Chihuahua
+  },
+  {
+    name: 'husky',
+    label: 'Husky',
+    image: Husky
+  },
+  {
+    name: 'pug',
+    label: 'Pug',
+    image: Pug
+  },
+  {
+    name: 'labrador',
+    label: 'Labrador',
+    image: Labrador
+  }
+];
+
+export default navigationItems;
