eebba8182ee7b38d23c6854ee6aa03144375413b
Luas1 <luciano.mamonas98@gmail.com>
2020-07-29 01:24:15 -0300

    Update Register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
+import { useFormik } from 'formik';
+
+import { AuthContext } from '../../contexts/AuthContext';
+
+import validationSchema from './ValidationSchema';
 
 import Form, { Group, Item, Label, TextField, Info } from '~/components/Form';
 import Button from '~/components/Button';
+import Spinner from '~/components/Spinner';
 
 import {
   Container,
@@ -15,8 +22,33 @@ import {
 } from './styles';
 
 const Register: React.FC = () => {
+  const { loading, handleLogin } = useContext(AuthContext);
+  const { navigate } = useNavigation();
+  const {
+    errors,
+    touched,
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit
+  } = useFormik({
+    initialValues: {
+      email: ''
+    },
+    validationSchema,
+    onSubmit: async (values, { resetForm }) => {
+      const { email } = values;
+
+      await handleLogin(email);
+      resetForm();
+    }
+  });
+
   return (
     <Container>
+      <AuthContext.Consumer>
+        {({ isAuthenticated }) => isAuthenticated && navigate('List')}
+      </AuthContext.Consumer>
       <Header>
         <Wrapper>
           <BackButton onPress={() => {}}>
@@ -32,17 +64,29 @@ const Register: React.FC = () => {
               <Item>
                 <Label>Email</Label>
                 <TextField
+                  keyboardType="email-address"
+                  autoCompleteType="email"
                   autoCapitalize="none"
+                  onBlur={handleBlur('email')}
+                  onChangeText={handleChange('email')}
+                  value={values.email}
                   placeholder="Digite seu email"
                 />
               </Item>
-              <Item>
-                <Info>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </Info>
-              </Item>
+              {touched.email && errors.email && (
+                <Item>
+                  <Info>{errors.email}</Info>
+                </Item>
+              )}
             </Group>
-            <Button buttonStyle="primary">Prosseguir</Button>
+            <Button
+              buttonStyle="primary"
+              onPress={(event: any) => handleSubmit(event)}
+              icon={() => <Spinner />}
+              disabled={loading}
+              iconShowLoading={loading}>
+              {!loading && <> Prosseguir</>}
+            </Button>
           </Form>
         </Content>
       </ContentWrapper>
