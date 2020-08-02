8f37ea3dda2b3bafb88f01f0888059e3d6da6711
Luas1 <luciano.mamonas98@gmail.com>
2020-08-02 05:19:45 -0300

    Gallery Columns 4 when width greater  400

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FlatList, Image, TouchableHighlight } from 'react-native';
+import { Dimensions } from 'react-native';
 
 import { Container } from './styles';
 
@@ -12,48 +13,33 @@ const Gallery: React.FC<GalleryProps> = ({ images, ...props }) => {
         data={images}
         bounces
         // extraData={images}
-        numColumns={2}
+        numColumns={Math.floor(Dimensions.get('window').width) <= 400 ? 2 : 4}
         collapsable
         debug={false}
         initialNumToRender={8}
         windowSize={2}
         showsVerticalScrollIndicator={false}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) =>
-          index % 2 === 0 ? (
-            <TouchableHighlight
-              style={{
+        renderItem={({ item, index }) => (
+          <TouchableHighlight
+            style={{
+              width: 170,
+              height: 170,
+              borderRadius: 10,
+              marginRight: 10,
+              marginBottom: 10
+            }}
+            onPress={() => console.log(index)}>
+            <Image
+              style={{ borderRadius: 10 }}
+              source={{
+                uri: item,
                 width: 170,
-                height: 170,
-                borderRadius: 10,
-                marginRight: 10,
-                marginBottom: 10
+                height: 170
               }}
-              onPress={() => console.log(index)}>
-              <Image
-                style={{ borderRadius: 10 }}
-                source={{
-                  uri: item,
-                  width: 170,
-                  height: 170
-                }}
-              />
-            </TouchableHighlight>
-          ) : (
-            <TouchableHighlight
-              style={{ width: 170, height: 170, borderRadius: 10 }}
-              onPress={() => console.log(index)}>
-              <Image
-                style={{ borderRadius: 10 }}
-                source={{
-                  uri: item,
-                  width: 170,
-                  height: 170
-                }}
-              />
-            </TouchableHighlight>
-          )
-        }
+            />
+          </TouchableHighlight>
+        )}
       />
     </Container>
   );
